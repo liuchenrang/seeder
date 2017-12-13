a9ba06243d9ad1b5cba03dff64e8692c0471dae8
xinghuo <xinghuo@In66.com>
2017-12-13 21:01:42 +0800

    update

--- stats/stats.go
+++ stats/stats.go
@@ -1,21 +1,27 @@
 package stats
 
 import (
+	"sync"
 	"sync/atomic"
 )
 
 type Stats struct {
-	total uint64
-	Stop  bool
+	muTotal sync.RWMutex
+	total   uint64
+	Stop    bool
 }
 
 //已分配数目
 func (stats *Stats) GetTotal() uint64 {
+	stats.muTotal.RLock()
+	defer stats.muTotal.RUnlock()
 	return stats.total
 }
 
 //分配计数
 func (stats *Stats) Dig() {
+	stats.muTotal.Lock()
+	defer stats.muTotal.Unlock()
 	pint := &stats.total
 	atomic.AddUint64(pint, 1)
 }
